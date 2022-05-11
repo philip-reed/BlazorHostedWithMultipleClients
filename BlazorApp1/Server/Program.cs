e32f62fadff7613ef56c7c3531bc4c352514dbaa
Philip Reed <Philip_Reed@next.co.uk>
2022-05-11 18:27:07 +0100

    fix from javiercn

--- BlazorApp1/Server/Program.cs
+++ BlazorApp1/Server/Program.cs
@@ -1,19 +1,67 @@
 using Microsoft.AspNetCore.ResponseCompression;
 
-var builder = WebApplication.CreateBuilder(args);
+//var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//builder.Services.AddControllersWithViews();
+//builder.Services.AddRazorPages();
+
+//var app = builder.Build();
+
+//var baseHref = "/demo";
+
+////app.MapGet("/", () => Results.Redirect(baseHref));
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseWebAssemblyDebugging();
+//}
+//else
+//{
+//    app.UseExceptionHandler("/Error");
+//}
+
+//app.UsePathBase(baseHref);
+
+//app.UseWhen(match =>
+//        match.Request.Path.StartsWithSegments(baseHref),
+//    demoApp =>
+//    {
+//        //This doesnt seem to make any difference or breaks the app completely
+//        //May need to remove <base href="/demo/"> from Index.html but doing so also doesnt seem to make a difference
+//        //demoApp.UsePathBase(baseHref);
+
+//        demoApp.UseRouting();
+//        demoApp.UseBlazorFrameworkFiles(baseHref);
+//        demoApp.UseStaticFiles();
+//        demoApp.UseStaticFiles(baseHref);
+        
+//        demoApp.UseEndpoints(endpoints =>
+//        {
+//            endpoints.MapFallbackToFile(
+//                baseHref + "/{*path:nonfile}",
+//                $"{baseHref}/index.html");
+//        });
+//    });
+
+//app.UseRouting();
+
+
+//app.MapRazorPages();
+//app.MapControllers();
+
+//app.Run();
+
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-var baseHref = "/demo";
-
-app.MapGet("/", () => Results.Redirect(baseHref));
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -23,31 +71,11 @@ else
     app.UseExceptionHandler("/Error");
 }
 
-app.UseWhen(match =>
-        match.Request.Path.StartsWithSegments(baseHref),
-    demoApp =>
-    {
-        //This doesnt seem to make any difference or breaks the app completely
-        //May need to remove <base href="/demo/"> from Index.html but doing so also doesnt seem to make a difference
-        //demoApp.UsePathBase(baseHref);
-
-        demoApp.UseRouting();
-        demoApp.UseBlazorFrameworkFiles(baseHref);
-        demoApp.UseStaticFiles();
-        demoApp.UseStaticFiles(baseHref);
-        
-        demoApp.UseEndpoints(endpoints =>
-        {
-            endpoints.MapFallbackToFile(
-                baseHref + "/{*path:nonfile}",
-                $"{baseHref}/index.html");
-        });
-    });
-
-app.UseRouting();
-
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
 
 app.MapRazorPages();
 app.MapControllers();
+app.MapFallbackToFile("/demo/{*path:nonfile}", "/demo/index.html");
 
-app.Run();
+await app.RunAsync();
